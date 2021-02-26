ce1b97995c84d22de4baa4227bcdf39d139059d8
thejellyfish <the@jellyfish-commuting.com>
2021-02-26 15:05:02 +0100

    Zzz ...

---
+++ index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+const __ = (...args) => args.join(' ');
+const __n = (...args) => args.join(' ');
+
+module.exports = { __, __n };
---
+++ index.test.js
@@ -0,0 +1,6 @@
+const { __ } = require('./index');
+
+// Start tests ...
+describe('Translate', () => {
+  it('Zzz ...', () => expect(__`Boom`).toBe('Boom'));
+});
