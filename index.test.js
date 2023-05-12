2702e4d0bb9775a664a7bfa789a15a9acf3f7b31
thejellyfish <the@jellyfish-commuting.com>
2023-05-12 10:18:12 +0200

    Update tests

--- index.test.js
+++ index.test.js
@@ -2,15 +2,16 @@ const __ = require('./index');
 
 // Start tests ...
 describe('Translate', () => {
-  it('Translate word not in dictionnary', () => expect(__`Boom`).toBe('Boom'));
+  it('Translate word not in dictionary', () => expect(__`Boom`).toBe('Boom'));
 
-  it('Set dictionnary and translate red in rouge', () => {
+  it('Set dictionary and translate "red" into "rouge"', () => {
     __.setMessages({ red: 'rouge' });
     expect(__`red`).toBe('rouge');
   });
 
-  it('Set dictionnary and translate pattern', () => {
-    __.setMessages({ 'My name is %s': 'Mon nom est %s' });
-    expect(__`My name is ${'Mohamed'}`).toBe('Mon nom est Mohamed');
+  it('Set dictionary and translate pattern "My name is %s %s"', () => {
+    __.setMessages({ 'My name is %s %s': 'Mon nom est %s %s' });
+    const me = { firstname: 'Mohamed', lastname: 'BENARROUDJ' };
+    expect(__`My name is ${me.firstname} ${me.lastname}`).toBe('Mon nom est Mohamed BENARROUDJ');
   });
 });
